fc6b9acdaa8db74a941ebe5a84571a999af30ab8
17 <2540705109@qq.com>
2018-12-09 21:14:03 +0800

    cloud

---
+++ early-autumn-eureka/src/main/java/com/early/autumn/eureka/EarlyAutumnEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.early.autumn.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EarlyAutumnEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EarlyAutumnEurekaApplication.class, args);
+    }
+}
