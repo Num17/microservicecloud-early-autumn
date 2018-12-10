0ec4db6b12a1f9b921711c04c581ea668589c8da
jiuhe <2540705109@qq.com>
2018-12-10 18:11:20 +0800

    hystrix dashboard

---
+++ early-autumn-hystrix-dashboard/src/main/java/com/early/autumn/earlyautumnhystrixdashboard/EarlyAutumnHystrixDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.early.autumn.earlyautumnhystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarlyAutumnHystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EarlyAutumnHystrixDashboardApplication.class, args);
+    }
+}
