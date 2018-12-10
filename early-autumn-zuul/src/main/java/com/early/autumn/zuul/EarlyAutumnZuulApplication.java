7454b3d3b4e3a9c8e3809d9b10f55f2a6a3e2790
jiuhe <2540705109@qq.com>
2018-12-10 17:45:09 +0800

    zuul

---
+++ early-autumn-zuul/src/main/java/com/early/autumn/zuul/EarlyAutumnZuulApplication.java
@@ -0,0 +1,12 @@
+package com.early.autumn.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarlyAutumnZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EarlyAutumnZuulApplication.class, args);
+    }
+}
