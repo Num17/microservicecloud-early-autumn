fba2539f7d906fc7319948201e041e98a445b8ad
jiuhe <2540705109@qq.com>
2018-12-12 11:02:04 +0800

    删除.gitignore文件

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # microservicecloud-early-autumn
 microservicecloud-early-autumn
+----early-autumn-api 所有服务端的client端
+----early-autumn-common 公共模块
+----early-autumn-eureka 服务注册中心
+----early-autumn-hystrix-dashboard 服务监控平台
+----early-autumn-user 用户服务
+----early-autumn-zuul 路由zuul
--- early-autumn-api/src/main/java/com/early/autumn/earlyautumnapi/config/RibbonConfig.java
+++ early-autumn-api/src/main/java/com/early/autumn/earlyautumnapi/config/RibbonConfig.java
@@ -1,17 +1,17 @@
-package com.early.autumn.user.config;
+package com.early.autumn.earlyautumnapi.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-//@Configuration
+@Configuration
 public class RibbonConfig {
 
-//    @Bean
-//    @LoadBalanced
-//    public RestTemplate restTemplate() {
-//        return new RestTemplate();
-//    }
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
